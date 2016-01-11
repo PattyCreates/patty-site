4196c31ac2119d92aad201da23c395c7ea7acc0d
Patty Castaneda <Designs@pattycreates.com>
2016-01-10 16:12:06 -0800

    updated scrolling js for accordion

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -1,22 +1,35 @@
 $(document).ready(function(){
-	//Accordian
+  var VERTICAL_PADDING = 30;
+  
+  //Accordian
+  $('.panel').on('show.bs.collapse', function () {
+    var $targetPanel = $(this);
+    var $lastPanel = $('.collapse.in').closest('.panel');
+    var panelList = $('.panel').toArray();
+    var topOffset = $(this).offset().top;
 
-  $('#accordion .panel-heading').on('click', function(){
-  	$('html,body').animate({scrollTop: $(this).offset().top}, 800);
-  }); 
+    if (panelList.indexOf($lastPanel[0]) > -1 && panelList.indexOf($lastPanel[0]) < panelList.indexOf($targetPanel[0])) {
+      var height = $lastPanel.outerHeight();
+      var closedHeight = $targetPanel.outerHeight();
+      $('html,body').scrollTop(topOffset - (height - closedHeight) - VERTICAL_PADDING);
+      // $('html,body').animate({scrollTop: topOffset - (height - closedHeight) - VERTICAL_PADDING}, 500);
+    } else {
+      $('html,body').animate({scrollTop: topOffset - VERTICAL_PADDING}, 500);
+    }
+  });
 
-  //Waypoints and animate.css 
- 	//Taiwan
+  //Waypoints and animate.css
+  //Taiwan
   // $('.ani-taiwan-1').waypoint(function() {
   //     $('.ani-taiwan-1').addClass('animated fadeInDown');
   // }, { offset: '0' });
-	if ($("#collapseFour").hasClass("in")) {
-		$('#taiwanA').waypoint(function() {
-	    $('#taiwanA').addClass('animated fadeInLeftBig');
-	  }, { offset: '0' });
+  if ($("#collapseFour").hasClass("in")) {
+    $('#taiwanA').waypoint(function() {
+      $('#taiwanA').addClass('animated fadeInLeftBig');
+    }, { offset: '0' });
   } else $('#taiwanA').waypoint(function() {
-	    $('#taiwanA').removeClass('animated fadeInLeftBig');
-	  }, { offset: '0' });
+      $('#taiwanA').removeClass('animated fadeInLeftBig');
+    }, { offset: '0' });
 
 
   $('#wine').waypoint(function() {
@@ -28,4 +41,4 @@ $(document).ready(function(){
   // $('.ani-taiwan-4').waypoint(function() {
   //     $('.ani-taiwan-4').addClass('animated fadeInRight');
   // }, { offset: '0' });
-});  
+});
