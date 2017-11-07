ac0222f968e3f6ce1dbde205438181467d799597
Patty Castaneda <Designs@pattycreates.com>
2017-11-07 18:18:45 +0200

    Removed smooth scroll. Added JS instead.

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -11,6 +11,25 @@
     // mixpanel.track_links(".accordion-6", "Clicked Art");
     // mixpanel.track_links(".accordion-7", "Clicked Maker");
 
+    //Navigation Scrolls
+    $(".work-nav").click(function() {
+      $('html, body').animate({
+        scrollTop: $("#work").offset().top
+      }, 500);
+    });
+
+    $(".about-nav").click(function() {
+      $('html, body').animate({
+        scrollTop: $("#about").offset().top
+      }, 800);
+    });
+
+    $(".connect-nav").click(function() {
+      $('html, body').animate({
+        scrollTop: $("#connect").offset().top
+      }, 1200);
+    });
+
     //Defining padding on breakpoints
     function scrollTopOfAccordion () {
 
